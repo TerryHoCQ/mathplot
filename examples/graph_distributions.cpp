403b093309ac3e0a9c7828f87aa131c34cd11cd5
Seb James <sebj_coder3@pm.me>
2025-08-05 15:11:27 +0100

    Omit Von Mises from Apple here, as PDF can't be computed

--- examples/graph_distributions.cpp
+++ examples/graph_distributions.cpp
@@ -125,6 +125,7 @@ int main()
     draw_graph (v, h, pd, sm::vec<>{gw, -gh},
                 std::format("Pareto {}={} {}={}", uc::toUtf8(uc::alpha), alpha, uc::toUtf8(uc::sigma), sigma));
 
+#ifndef __APPLE__
     // Von Mises distribution
     mu = F{0};
     kappa = F{0.7};
@@ -136,6 +137,7 @@ int main()
     }
     draw_graph (v, h, pd, sm::vec<>{2 * gw, -gh},
                 std::format("Von Mises {}={} {}={}", uc::toUtf8(uc::mu), mu, uc::toUtf8(uc::kappa), kappa));
+#endif
 
     v.keepOpen();
 
