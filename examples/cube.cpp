5f530991b9c6c2caf0399d0708b6dc7f5646b7f0
Seb James <sebj_coder3@pm.me>
2025-09-25 13:26:53 +0100

    WIP

--- examples/cube.cpp
+++ examples/cube.cpp
@@ -14,15 +14,16 @@ int main()
     // Parameters of the model
     sm::vec<float, 3> offset = { 1, -0.5, -0.5 };   // a within-scene offset
 
-    sm::vec<float, 3> e1 =     { 1, 0, 0 };
-    sm::vec<float, 3> e2 =     { 0, 1, 0 };
-    sm::vec<float, 3> e3 =     { 0, 0, 1 };
+    sm::vec<float, 3> e1 = { 1, 0, 0 };
+    sm::vec<float, 3> e2 = { 0, 1, 0 };
+    sm::vec<float, 3> e3 = { 0, 0, 1 };
 
     sm::vec<float, 3> colour1 = { 0.35,  0.76,  0.98 };  // RGB colour triplet
 
     auto rv = std::make_unique<mplot::RhomboVisual<>> (offset, e1, e2, e3, colour1);
     v.bindmodel (rv);
     rv->name = "Cube.002";
+    rv->facecm = mplot::ColourMapType::Magma;
     rv->finalize();
     v.addVisualModel (rv);
 
