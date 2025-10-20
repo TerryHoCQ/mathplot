a56a2f5345b31663cdf61ea960f3d43936fbdc30
Seb James <sebj_coder3@pm.me>
2025-10-20 15:53:57 +0100

    Show normals for both rods

--- examples/rod_with_normals.cpp
+++ examples/rod_with_normals.cpp
@@ -30,27 +30,33 @@ int main()
 
     try {
         constexpr sm::vec<float, 3> colour1 = { 1.0, 0.0, 0.0 };
-        constexpr sm::vec<float, 3> colour2 = { 0.0, 0.9, 0.4 };
-
         sm::vec<float, 3> offset = { 0.0, 0.0, 0.0 };
         sm::vec<float, 3> start = { 0, 0, 0 };
         sm::vec<float, 3> end = { 0.25, 0, 0 };
-        std::unique_ptr<mplot::VisualModel<>> rvm = std::make_unique<mplot::RodVisual<>> (offset, start, end, 0.1f, colour1, colour1);
+        auto rvm = std::make_unique<mplot::RodVisual<>> (offset, start, end, 0.1f, colour1, colour1);
         v.bindmodel (rvm);
+        rvm->use_oriented_tube = false;
         rvm->finalize();
-        v.addVisualModel (rvm);
+        auto rvmp1 = v.addVisualModel (rvm);
+        rvmp1->vertex_postprocess();
 
+        auto nrm = std::make_unique<mplot::NormalsVisual<>> (rvmp1);
+        v.bindmodel (nrm);
+        nrm->finalize();
+        v.addVisualModel (nrm);
+
+        constexpr sm::vec<float, 3> colour2 = { 0.0, 0.9, 0.4 };
         sm::vec<float, 3> start2 = { -0.1, 0.2, 0.6 };
         sm::vec<float, 3> end2 = { 0.2, 0.4, 0.6 };
         // You can reuse the unique_ptr rvm, once you've transferred ownership with v.addVisualModel (rvm)
         rvm = std::make_unique<mplot::RodVisual<>>(offset, start2, end2, 0.05f, colour2);
         v.bindmodel (rvm);
         rvm->finalize();
-        auto rvmp = v.addVisualModel (rvm);
-        rvmp->vertex_postprocess();
+        auto rvmp2 = v.addVisualModel (rvm);
+        rvmp2->vertex_postprocess();
 
         // Create an associate normals model
-        auto nrm = std::make_unique<mplot::NormalsVisual<>> (rvmp);
+        nrm = std::make_unique<mplot::NormalsVisual<>> (rvmp2);
         v.bindmodel (nrm);
         nrm->finalize();
         v.addVisualModel (nrm);
