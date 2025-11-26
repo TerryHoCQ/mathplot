85e9be29ddfc3a54eba64dea24f9751ec0caea3d
Seb James <sebj_coder3@pm.me>
2025-11-26 10:41:19 +0000

    Adds spherical projections to healpix.cpp

--- examples/healpix.cpp
+++ examples/healpix.cpp
@@ -7,6 +7,7 @@
 #include <sm/vec>
 #include <mplot/Visual.h>
 #include <mplot/HealpixVisual.h>
+#include <mplot/SphericalProjectionVisual.h>
 
 int main (int argc, char** argv)
 {
@@ -37,7 +38,60 @@ int main (int argc, char** argv)
 
     // Finalize and add
     hpv->finalize();
-    v.addVisualModel (hpv);
+    auto hpvp = v.addVisualModel (hpv);
+
+    // Show some 2D projections, as well
+    sm::vvec<std::array<float, 3>> hpvcolours (hpvp->pixeldata.size(), mplot::colour::crimson);
+    sm::vvec<sm::vec<float, 2>> latlong (hpvp->pixeldata.size());
+    for (uint32_t i = 0; i < hpvp->pixeldata.size(); ++i) {
+
+        hp::t_ang ang = hp::nest2ang (hpvp->get_nside(), i);
+
+        // ang.theta is co-latitude (and needs re-casting to be longitude), ang.phi is a longitude
+        float lat = ang.theta - sm::mathconst<float>::pi_over_2;
+        // constrain to range -pi/2 < lat <= pi/2
+        lat *= 2.0f;
+        sm::algo::minus_pi_to_pi (lat);
+        lat /= 2.0f;
+
+        latlong[i] = { lat, static_cast<float>(ang.phi) };
+
+        hpvcolours[i] = hpvp->cm.convert (hpvp->pixeldata[i]);
+    }
+
+    // Add two-dimensional projections
+    auto spv = std::make_unique<mplot::SphericalProjectionVisual<float>> (sm::vec<float>{4,0,0});
+    v.bindmodel (spv);
+    spv->twodimensional (true);
+    spv->proj_type = sm::geometry::spherical_projection::type::mercator;
+    spv->latlong = latlong;
+    spv->colour = hpvcolours;
+    spv->finalize();
+    auto spvp = v.addVisualModel (spv);
+    auto ext = spvp->extents(); // Use VisualModel::extents() to help place the label
+    spvp->addLabel ("Mercator projection", sm::vec<>{ ext[0].min, ext[1].min - 0.16f, 0.0f }, mplot::TextFeatures(0.08f));
+
+    spv = std::make_unique<mplot::SphericalProjectionVisual<float>> (sm::vec<float>{-5,-4,0});
+    v.bindmodel (spv);
+    spv->twodimensional (true);
+    spv->proj_type = sm::geometry::spherical_projection::type::equirectangular;
+    spv->latlong = latlong;
+    spv->colour = hpvcolours;
+    spv->finalize();
+    spvp = v.addVisualModel (spv);
+    ext = spvp->extents();
+    spvp->addLabel ("Equirectangular projection", sm::vec<>{ ext[0].min, ext[1].min - 0.16f, 0.0f }, mplot::TextFeatures(0.08f));
+
+    spv = std::make_unique<mplot::SphericalProjectionVisual<float>> (sm::vec<float>{-4,3,0});
+    v.bindmodel (spv);
+    spv->twodimensional (true);
+    spv->proj_type = sm::geometry::spherical_projection::type::cassini;
+    spv->latlong = latlong;
+    spv->colour = hpvcolours;
+    spv->finalize();
+    spvp = v.addVisualModel (spv);
+    ext = spvp->extents();
+    spvp->addLabel ("Cassini projection", sm::vec<>{ ext[0].min, ext[1].min - 0.16f, 0.0f }, mplot::TextFeatures(0.08f));
 
     v.keepOpen();
     return 0;
