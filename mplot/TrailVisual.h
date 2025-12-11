b134b33ce3ad98eef77322fda921a484d2ddaaf5
Seb James <sebj_coder3@pm.me>
2025-12-11 17:03:21 +0000

    Skeleton.

---
+++ mplot/TrailVisual.h
@@ -0,0 +1,4 @@
+/*
+ * A 'breadcrumb trail'. A bit like ScatterVisual, but with a fixed size of data container. Copies
+ * of a sphere are used for the crumbs - instanced rendering. Will need some changes in core mathplot for this
+ */
--- mplot/VisualModelBase.h
+++ mplot/VisualModelBase.h
@@ -65,6 +65,7 @@ namespace mplot
         twodimensional,         // If true, then this VisualModel should always be viewed in a plane - it's a 2D model
         hide,                   // If true, then calls to VisualModel::render should return
         wireframe,              // If true, draw in GL's polygon mode
+        instance_model,         // If true, draw this as an array instance model
         show_bb,                // If true, draw vertices/indices for the bounding box frame
         compute_bb              // For some models, it's not useful to compute the bounding box (e.g. coordinate arrows)
     };
