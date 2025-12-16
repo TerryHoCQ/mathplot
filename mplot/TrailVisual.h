50c06264a58cee46c15d66cd54896200189ccc76
Seb James <sebj_coder3@pm.me>
2025-12-16 17:22:53 +0000

    Was a skeleton, but haven't used it

--- mplot/TrailVisual.h
+++
@@ -1,4 +0,0 @@
-/*
- * A 'breadcrumb trail'. A bit like ScatterVisual, but with a fixed size of data container. Copies
- * of a sphere are used for the crumbs - instanced rendering. Will need some changes in core mathplot for this
- */
