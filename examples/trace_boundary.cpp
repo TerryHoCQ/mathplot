4ad2c643711d99f0b0139fa35f05f533274bd150
Seb James <sebj_coder3@pm.me>
2026-01-20 11:16:36 +0000

    Updates to trace boundary

--- examples/trace_boundary.cpp
+++ examples/trace_boundary.cpp
@@ -1,5 +1,6 @@
 /*
- * Trace around a boundary of points in 2D
+ * Trace around a boundary of points in 2D. This code uses Graham's scan to compute the convex hull
+ * of a set of randomly generated points.
  */
 #include <iostream>
 
@@ -16,7 +17,7 @@
 int main()
 {
     // Get ready to graph the data
-    mplot::Visual v(1024, 768, "Boundary tracing");
+    mplot::Visual v(1024, 768, "Graham's scan computes the convex hull");
     mplot::DatasetStyle ds (mplot::stylepolicy::markers);
     auto gv = std::make_unique<mplot::GraphVisual<float>> (sm::vec<float>{-0.5f,-0.5f,0.0f});
     v.bindmodel (gv);
@@ -24,23 +25,22 @@ int main()
     constexpr unsigned int n_points = 200;
 
     // Create data
-    sm::rand_uniform<float> rngxy(-0.8f, 0.8f, n_points);
+    sm::rand_uniform<float> rngxy(-0.8f, 0.8f);
     sm::vvec<sm::vec<float, 2>> points(n_points);
     for (unsigned int i = 0; i < n_points; ++i) { points[i] = { rngxy.get(), rngxy.get() }; }
 
     // Trace algorithm
     sm::vvec<sm::vec<float, 2>> boundary = sm::geometry::graham_scan (points);
+    // Close the boundary
+    boundary.push_back (boundary.front());
 
-    gv->setlimits (-1.5, 1.5, -1.5, 1.5);
+    gv->setlimits (-1, 1, -1, 1);
 
     gv->setdata (points, ds);
 
-    ds.showlines = true;
-    ds.linecolour = mplot::colour::crimson;
-    ds.markercolour = mplot::colour::black;
-    ds.markerstyle = mplot::markerstyle::circle;
-    ds.markersize *= 1.2f;
-    gv->setdata (boundary, ds);
+    mplot::DatasetStyle ds2 (mplot::stylepolicy::lines);
+    ds2.linecolour = mplot::colour::crimson;
+    gv->setdata (boundary, ds2);
 
     gv->finalize();
     v.addVisualModel (gv);
