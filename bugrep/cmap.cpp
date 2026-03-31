df97029bd102f3d1bff681022fe7e4c1324a9386
Seb James <sebj_coder3@pm.me>
2026-03-31 14:35:27 +0100

    Bug fix in bugrep program, add -freport-bug if BUILD_BUGREP is ON

--- bugrep/cmap.cpp
+++ bugrep/cmap.cpp
@@ -6,5 +6,5 @@ int main()
 {
     mplot::ColourMap<float> cm (mplot::ColourMapType::Plasma);
     std::array<float, 3> c = cm.convert (0.5f);
-    std::cout << "Colour is (R,G,B): (" << c[0] << "," << c[1] << "," c[2] << "\n";
+    std::cout << "Colour is (R,G,B): (" << c[0] << "," << c[1] << "," << c[2] << "\n";
 }
