89fb8e50fe2623df64a7014ec9fc9a0b10e3e203
Seb James <sebj_coder3@pm.me>
2026-05-27 14:34:07 +0100

    Triangle example

--- examples/tri.cpp
+++ examples/tri.cpp
@@ -2,9 +2,11 @@
  * Visualize a triangle
  */
 #include <iostream>
-#include <sm/vec>
-#include <mplot/Visual.h>
-#include <mplot/TriangleVisual.h>
+#include <memory>
+
+import sm.vec;
+import mplot.visual;
+import mplot.trianglevisual;
 
 int main()
 {
@@ -28,7 +30,7 @@ int main()
         sm::vec<float, 3> colour1 = { 1.0f, 0.0f, 0.0f };
 
         auto tv = std::make_unique<mplot::TriangleVisual<>> (offset, c1, c2, c3, colour1);
-        v.bindmodel (tv);
+        tv->set_parent (v.get_id());
         tv->finalize();
         v.addVisualModel (tv);
 
