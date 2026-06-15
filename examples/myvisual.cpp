a969eca43f6400176e0413550d3d34686da4965f
Seb James <sebj_coder3@pm.me>
2026-06-15 15:47:53 +0100

    Better set of examples of how to use keys.cppm

--- examples/myvisual.cpp
+++ examples/myvisual.cpp
@@ -35,6 +35,7 @@ protected:
 
         return false; // No need to re-render the window for either option
     }
+
     // Also optionally, add actions for extra keys:
     static constexpr bool debug_callback_extra = false;
     void key_callback_extra (int key, [[maybe_unused]] int scancode, int action, [[maybe_unused]] int mods) override
@@ -43,12 +44,69 @@ protected:
             std::cout << "myvisual::key_callback_extra called for key=" << key << " scancode="
                       << scancode << " action=" << action << " and mods=" << mods << std::endl;
         }
-        // 'f' key means toggle the 'moving' attribute
-        if (key == mplot::key::f && action == mplot::keyaction::press) { this->moving = this->moving ? false : true; }
 
+        // As an example, bind the 'f' key to toggle the 'moving' attribute
+        if (key == mplot::key::f && action == mplot::keyaction::press) {
+            this->moving = this->moving ? false : true;
+        }
+
+        // You can require shift and a key like this:
+        if (key == mplot::key::k && (mods & keymod::shift) && action == mplot::keyaction::press) {
+            std::cout << "Shift-k was pressed\n";
+        }
+
+        // Ctrl-key is:
+        if (key == mplot::key::k && (mods & keymod::control) && action == mplot::keyaction::press) {
+            std::cout << "Ctrl-k was pressed\n";
+        }
+
+        // combined:
+        if (key == mplot::key::k
+            && (mods & (keymod::shift | keymod::control)) == (keymod::shift | keymod::control)
+            && action == mplot::keyaction::press) {
+            std::cout << "Shift-Ctrl-k was pressed\n";
+        }
+
+        // Other keymod bits that you can use are alt and super:
+        if (key == mplot::key::k && (mods & keymod::alt) && action == mplot::keyaction::press) {
+            std::cout << "Alt-k was pressed\n";
+        }
+        if (key == mplot::key::k && (mods & keymod::super) && action == mplot::keyaction::press) {
+            std::cout << "Super-k was pressed\n";
+        }
+
+        // In theory, mods can hold the status of caps_lock and num_lock. Doesn't work for Seb.
+        if ((mods & keymod::caps_lock) && action == mplot::keyaction::press) {
+            std::cout << "A key was pressed and Caps Lock was active\n";
+        }
+        if ((mods & keymod::num_lock) && action == mplot::keyaction::press) {
+            std::cout << "A key was pressed and Num Lock was active\n";
+        }
+
+        // The caps lock key is also treated just as a key (works for Seb):
+        if (key == mplot::key::caps_lock && action == mplot::keyaction::press) {
+            std::cout << "Caps Lock was pressed\n";
+        }
+
+        // As well as press, you can test on release or repeat
+        if (action == mplot::keyaction::release) {
+            if (key == mplot::key::caps_lock) {
+                std::cout << "Caps-lock released\n";
+            } else {
+                std::cout << "Key released\n";
+            }
+        }
+        if (action == mplot::keyaction::repeat) {
+            std::cout << "Key repeat\n";
+        }
+
+        // Add some additional help output:
         if (key == mplot::key::h && action == mplot::keyaction::press) {
             std::cout << "myvisual extra help:\n";
             std::cout << "f: 'move'\n";
+            std::cout << "Shift-k: output a message to stdout\n";
+            std::cout << "Ctrl-k: output a message to stdout\n";
+            std::cout << "Any key release or repeat: output a message to stdout\n";
         }
     }
 };
@@ -56,7 +114,7 @@ protected:
 int main()
 {
     myvisual v(600, 400, "Custom Visual: myvisual");
-    v.addLabel ("Hello World! Try the key 'f' (then look at stdout)", {0,0,0});
+    v.addLabel ("Hello World! Try the key 'f' (then look at stdout). Try Shift-k or Ctrl-k", {0,0,0});
     while (!v.readyToFinish()) {
         v.waitevents (0.018);
         if (v.moving == true) {
